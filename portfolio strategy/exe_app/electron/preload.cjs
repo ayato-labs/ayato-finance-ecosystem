b01ee85b7e62f0001a66439214197d5dcf361529
ayato-labs <ayato-labs@users.noreply.github.com>
2026-07-08 22:02:10 +0900

    refactor: relocate out-of-scope applications to parent directory (#56)

--- portfolio strategy/exe_app/electron/preload.cjs
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  log: (level, msg, context) => ipcRenderer.send('log', { level, msg, context })
-});
